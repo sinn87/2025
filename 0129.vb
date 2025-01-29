9a5ee33069eb4422129cef4890ba2e9cf1f76406
sinn87 <sinn87@users.noreply.github.com>
2025-01-29 09:51:41 +0900

    Create 0129.vb

---
+++ 0129.vb
@@ -0,0 +1,38 @@
+Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 调用方法填充 ComboBox
+        LoadComboBoxData()
+    End Sub
+
+    Private Sub LoadComboBoxData()
+        ' 数据库连接字符串，请替换为你的实际连接信息
+        Dim connStr As String = "Server=你的服务器地址;Database=你的数据库名;User Id=你的用户名;Password=你的密码;"
+        
+        ' SQL 查询（假设第三列名为 column3）
+        Dim query As String = "SELECT column3 FROM a"
+
+        ' 创建数据库连接
+        Using conn As New SqlConnection(connStr)
+            Try
+                conn.Open()
+                ' 创建命令
+                Using cmd As New SqlCommand(query, conn)
+                    ' 执行查询
+                    Using reader As SqlDataReader = cmd.ExecuteReader()
+                        ' 清空 ComboBox 以防止重复加载
+                        ComboBox1.Items.Clear()
+
+                        ' 读取数据并填充 ComboBox
+                        While reader.Read()
+                            ComboBox1.Items.Add(reader("column3").ToString())
+                        End While
+                    End Using
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("数据库连接错误：" & ex.Message)
+            End Try
+        End Using
+    End Sub
+End Class
